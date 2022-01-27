2868885c0e1b2c1cf00dbd60d7e44ae7f0eadb6b
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2022-01-27 09:08:05 +0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.jsx',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
